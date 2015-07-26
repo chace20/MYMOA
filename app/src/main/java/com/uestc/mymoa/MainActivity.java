2e9fc9b0332c02686388c4a1051282f05c8bab79
nothisboy <nothiboy@github.com>
2015-07-26 10:55:30 +0800

    coding main activity

--- app/src/main/java/com/uestc/mymoa/MainActivity.java
+++ app/src/main/java/com/uestc/mymoa/MainActivity.java
@@ -1,7 +1,8 @@
 package com.uestc.mymoa;
 
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
@@ -12,11 +13,17 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.layout_main_base);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar.setTitle("Main");
+        setSupportActionBar(toolbar);
+//        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main_index, menu);
         return true;
     }
@@ -35,4 +42,5 @@ public class MainActivity extends ActionBarActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
 }

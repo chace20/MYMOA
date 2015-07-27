c90e1a1ba0cd394bf357654a8d082489677ad627
nothisboy <nothiboy@github.com>
2015-07-27 16:11:58 +0800

    add viewpager

---
+++ app/src/main/java/com/uestc/mymoa/ContactFragment.java
@@ -0,0 +1,19 @@
+package com.uestc.mymoa;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by nothisboy on 2015/7/26.
+ */
+public class ContactFragment extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = getActivity().getLayoutInflater().inflate(R.layout.layout_main_contact_fragment, null);
+
+        return view;
+    }
+}
---
+++ app/src/main/java/com/uestc/mymoa/MessageFragment.java
@@ -0,0 +1,22 @@
+package com.uestc.mymoa;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by nothisboy on 2015/7/26.
+ */
+public class MessageFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = getActivity().getLayoutInflater().inflate(R.layout.layout_main_message_fragment, null);
+
+
+        return view;
+    }
+}

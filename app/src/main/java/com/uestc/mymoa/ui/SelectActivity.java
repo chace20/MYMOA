172c91befdf05759cca6d266a834ec7b1217fb8d
chao <chao9420@gmail.com>
2015-07-27 16:26:34 +0800

    del .idea

---
+++ app/src/main/java/com/uestc/mymoa/ui/SelectActivity.java
@@ -0,0 +1,28 @@
+package com.uestc.mymoa.ui;
+
+import com.uestc.mymoa.R;
+
+/**
+ * Created by chao on 2015/7/27.
+ */
+public class SelectActivity extends BaseActivity {
+    @Override
+    protected void initLayout() {
+
+    }
+
+    @Override
+    protected void initListener() {
+
+    }
+
+    @Override
+    protected void initValue() {
+
+    }
+
+    @Override
+    protected int setRootView() {
+        return R.layout.layout_select;
+    }
+}

1cd6f8a0247b83a6b0b885661ed57e70b0c0f645
nothisboy <nothiboy@github.com>
2015-07-28 20:57:36 +0800

    nothing

--- app/src/main/java/com/uestc/mymoa/io/DocSearchDocHandler.java
+++ app/src/main/java/com/uestc/mymoa/io/DocSearchDocHandler.java
@@ -1,7 +1,66 @@
 package com.uestc.mymoa.io;
 
+import android.widget.ListView;
+
+import com.lidroid.xutils.HttpUtils;
+import com.lidroid.xutils.exception.HttpException;
+import com.lidroid.xutils.http.RequestParams;
+import com.lidroid.xutils.http.ResponseInfo;
+import com.lidroid.xutils.http.callback.RequestCallBack;
+import com.lidroid.xutils.http.client.HttpRequest;
+import com.uestc.mymoa.constant.Api;
+import com.uestc.mymoa.io.model.DocContent;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by nothisboy on 2015/7/28.
  */
-public class DocSearchDocHandler {
+public class DocSearchDocHandler extends IOHandler {
+    protected IOCallback callback;
+
+    @Override
+    public void process(RequestParams params, IOCallback ioCallback) {
+        this.callback = ioCallback;
+        HttpUtils http = new HttpUtils();
+        http.send(HttpRequest.HttpMethod.GET, Api.Doc.searchDoc, params, new RequestCallBack<String>() {
+            @Override
+            public void onStart() {
+                callback.onStart();
+            }
+
+            @Override
+            public void onLoading(long total, long current, boolean isUploading) {
+            }
+
+            @Override
+            public void onSuccess(ResponseInfo<String> responseInfo) {
+                try {
+                    List<DocContent> list = new ArrayList<DocContent>();
+                    JSONArray jsonArray = new JSONArray(responseInfo);
+                    for (int i = 0; i < jsonArray.length(); i++) {
+                        JSONObject jsonObject = jsonArray.getJSONObject(i);
+                        DocContent docContent = new DocContent();
+                        docContent.docid = (String) jsonObject.get("docid");
+                        docContent.title = (String) jsonObject.get("title");
+                        list.add(docContent);
+                    }
+                    callback.onSuccess(list);
+
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            @Override
+            public void onFailure(HttpException e, String s) {
+                callback.onFailure(s);
+            }
+        });
+    }
 }

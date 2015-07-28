f27e583dddc5cf9886425b5b616b49d7ce9c8402
nothisboy <nothiboy@github.com>
2015-07-28 16:21:35 +0800

    add some handler and some intent

---
+++ app/src/main/java/com/uestc/mymoa/io/model/NewsContent.java
@@ -0,0 +1,12 @@
+package com.uestc.mymoa.io.model;
+
+/**
+ * Created by nothisboy on 2015/7/28.
+ */
+public class NewsContent {
+
+    public String newsid;
+    public String title;
+    public String uname;
+    public String content;
+}
